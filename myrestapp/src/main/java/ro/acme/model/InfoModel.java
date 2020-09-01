b4a4abdc6e7db21c8bbae8a7d42a2f2f074783ae
b.andronache <b.andronache@gmail.com>
2020-09-01 13:06:33 +0300

    view cwertificates

--- myrestapp/src/main/java/ro/acme/model/InfoModel.java
+++ myrestapp/src/main/java/ro/acme/model/InfoModel.java
@@ -1,14 +1,21 @@
 package ro.acme.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import lombok.Data;
 
 @Data
 public class InfoModel {
 
 	private String message;
-	
+
 	private String ldapUser;
-	
+
 	private String ldapPassword;
+
+	private Map<String, String> headers = new HashMap<>();
+
+	private String certificate;
 
 }
--- myrestapp/src/main/java/ro/acme/web/Myrestapi.java
+++ myrestapp/src/main/java/ro/acme/web/Myrestapi.java
@@ -1,10 +1,16 @@
 package ro.acme.web;
 
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,13 +29,28 @@ public class Myrestapi {
 	private AcmeCommonProperties commonProperties;
 
 	@GetMapping(path = "/appinfo", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<InfoModel> appInfo() {
+	public ResponseEntity<InfoModel> appInfo(@RequestHeader Map<String, String> headers, HttpServletRequest request) {
 
 		InfoModel im = new InfoModel();
 		im.setMessage(commonProperties.getMessage());
-		
+
 		im.setLdapUser(ldapProperties.getUser());
 		im.setLdapPassword(ldapProperties.getPassword());
+		Map<String, String> msgHeaders = im.getHeaders();
+
+		headers.forEach((key, value) -> {
+			msgHeaders.put(key, value);
+		});
+		
+		
+		
+
+		X509Certificate[] certificates = (X509Certificate[]) request
+				.getAttribute("javax.servlet.request.X509Certificate");
+
+		if (certificates != null && certificates.length > 0) {
+			im.setCertificate(certificates[0].toString());
+		}
 
 		return new ResponseEntity<>(im, HttpStatus.OK);
 	}
